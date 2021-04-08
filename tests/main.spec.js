94c2ee0d625640f5d80690cecc88e8b41c9faf2b
Paulo Valadares <contato@paulovaladares.com.br>
2021-04-08 00:42:31 -0300

    First CLI test - Hello Worl

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,7 +1,19 @@
-import { expect } from 'chai';
+/* eslint-disable spaced-comment */
+const expect = require('chai').expect;
+const exec = require('child_process').exec;
+const btcConverter = './src/main.js'; // chmod+x file to permit executable
 
-describe('Main', () => {
-  it('Should return true', () => {
-    expect(true).to.equal(true);
+describe('Man CLI', () => {
+  // parameter "done" when async
+  it('should retunr Hello World', done => {
+    /*
+     * exec(file to execute, callback w/ 3 parameters:
+     * ("error", "output of terminal", "output error") => {}
+     */
+    exec(btcConverter, (err, stdout, stderr) => {
+      if (err) throw err;
+      expect(stdout.replace('\n', '')).to.equal('Hello World!');
+      done();
+    });
   });
 });
