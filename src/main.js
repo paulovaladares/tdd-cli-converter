4181b8c2be2f93bac545bd6d43fdab6a50abd16d
Paulo Valadares <contato@paulovaladares.com.br>
2021-04-08 23:57:54 -0300

    Added commander

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 
-console.log('Hello World!');
+const program = require('commander');
+const pkg = require('../package.json');
+
+program.version(pkg.version).description(pkg.description).parse(process.argv);
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,18 +1,28 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable spaced-comment */
 const expect = require('chai').expect;
 const exec = require('child_process').exec;
 const btcConverter = './src/main.js'; // chmod+x file to permit executable
+const pkg = require('../package.json');
 
 describe('Man CLI', () => {
   // parameter "done" when async
-  it('should retunr Hello World', done => {
+  it('should retunr the version when run --version', done => {
     /*
-     * exec(file to execute, callback w/ 3 parameters:
+     * exec(command to execute, callback w/ 3 parameters:
      * ("error", "output of terminal", "output error") => {}
      */
-    exec(btcConverter, (err, stdout, stderr) => {
+    exec(`${btcConverter} --version`, (err, stdout, st) => {
       if (err) throw err;
-      expect(stdout.replace('\n', '')).to.equal('Hello World!');
+      expect(stdout.replace('\n', '')).to.equal(pkg.version);
+      done();
+    });
+  });
+
+  it('should return a description when run help', done => {
+    exec(`${btcConverter} --help`, (err, stdout, st) => {
+      expect(stdout.includes('Cli converter for / to any currencies')).to.be
+        .true;
       done();
     });
   });
